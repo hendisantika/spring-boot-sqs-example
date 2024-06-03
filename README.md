28786ce6c66368fdedea7cc7f8604d6b524c34e4
hendisantika <hendisantika@yahoo.co.id>
2024-06-03 08:00:19 +0700

    Create Spring Boot App 🫸🌀✏️📗🐧🐳

---
+++ README.md
@@ -0,0 +1,1 @@
+# spring-boot-sqs-example
---
+++ src/main/java/id/my/hendisantika/sqsexample/SpringBootSqsExampleApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.sqsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSqsExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSqsExampleApplication.class, args);
+    }
+
+}
