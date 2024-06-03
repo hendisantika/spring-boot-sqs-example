be6eedebb51b45d8676da9a9d78c41c61bbfa365
hendisantika <hendisantika@yahoo.co.id>
2024-06-03 08:09:50 +0700

    Add processMessage in OrderMessageListener🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/sqsexample/listener/OrderMessageListener.java
+++ src/main/java/id/my/hendisantika/sqsexample/listener/OrderMessageListener.java
@@ -1,8 +1,11 @@
 package id.my.hendisantika.sqsexample.listener;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import id.my.hendisantika.sqsexample.dto.OrderDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
+import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.stereotype.Component;
 
@@ -22,4 +25,17 @@ import org.springframework.stereotype.Component;
 public class OrderMessageListener {
     private static final ObjectMapper OBJECT_MAPPER = Jackson2ObjectMapperBuilder.json().build();
     private final OrderService orderService;
+
+    @SqsListener(value = "${orders.queue.name}", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
+    public void processMessage(String message) {
+        try {
+            log.debug("Received new SQS message: {}", message);
+            OrderDto orderDto = OBJECT_MAPPER.readValue(message, OrderDto.class);
+
+            this.orderService.processOrder(orderDto);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot process message from SQS", e);
+        }
+    }
 }
