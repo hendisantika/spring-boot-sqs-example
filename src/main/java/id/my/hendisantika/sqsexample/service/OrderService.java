1e3862ce813fdb8ab068a03136c244aa167135fd
hendisantika <hendisantika@yahoo.co.id>
2024-06-03 08:12:58 +0700

    Add placeOrder in OrderService🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/sqsexample/service/OrderService.java
+++ src/main/java/id/my/hendisantika/sqsexample/service/OrderService.java
@@ -1,10 +1,17 @@
 package id.my.hendisantika.sqsexample.service;
 
 import id.my.hendisantika.sqsexample.dto.OrderDto;
+import id.my.hendisantika.sqsexample.enums.MessageType;
 import id.my.hendisantika.sqsexample.producer.SqsMessageProducer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -29,5 +36,18 @@ public class OrderService {
         not going to do that here, thats not the point of this demo app
 
          */
+    }
+
+    public OrderDto placeOrder(OrderDto orderDto) {
+        log.debug(" {} Place order {} {}", orderDto);
+        orderDto.setOrderId(UUID.randomUUID());
+        orderDto.setOrderDate(new Date());
+
+        Map<String, Object> headers = new HashMap<>();
+        headers.put("Message-Type", MessageType.ORDER.name());
+        headers.put("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+        log.debug("{} Send message to Queue {} ");
+        this.producer.send(orderDto, headers);
+        return orderDto;
     }
 }
