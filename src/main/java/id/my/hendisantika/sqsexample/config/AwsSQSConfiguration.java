45c33fdc06bdb270d98467a7fc06f306e7e28161
hendisantika <hendisantika@yahoo.co.id>
2024-06-03 08:05:17 +0700

    Add queueMessagingTemplate in AwsSQSConfiguration 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/sqsexample/config/AwsSQSConfiguration.java
+++ src/main/java/id/my/hendisantika/sqsexample/config/AwsSQSConfiguration.java
@@ -6,6 +6,7 @@ import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.config.annotation.EnableSqs;
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -39,6 +40,11 @@ public class AwsSQSConfiguration {
     @Bean
     public AWSCredentialsProvider credentialsProvider() {
         return new DefaultAWSCredentialsProviderChain();
+    }
+
+    @Bean
+    public QueueMessagingTemplate queueMessagingTemplate() {
+        return new QueueMessagingTemplate(amazonSQSAsync());
     }
 
 }
