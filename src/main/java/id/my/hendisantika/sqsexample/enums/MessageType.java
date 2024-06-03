055aa0af633a154a5cface03adc2588dc4d324f2
hendisantika <hendisantika@yahoo.co.id>
2024-06-03 08:07:41 +0700

    Add MessageType Enum🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/sqsexample/enums/MessageType.java
@@ -0,0 +1,15 @@
+package id.my.hendisantika.sqsexample.enums;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-sqs-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/3/24
+ * Time: 08:07
+ * To change this template use File | Settings | File Templates.
+ */
+public enum MessageType {
+    ORDER
+}
